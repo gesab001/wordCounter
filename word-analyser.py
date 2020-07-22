c65d0140712f1d88ed5c94c97b8a00a8821c0336
gesab001 <144000giovanni@gmail.com>
2020-07-23 03:28:25 +1200

    added all books to vocabulary.json

--- word-analyser.py
+++ word-analyser.py
@@ -9,61 +9,62 @@ keystodelete = ["1 Samuel", "2 Samuel", "Daniel", "Deuteronomy", "Exodus", "Gene
 for key in keystodelete:
    booklist.remove(key)
 print(booklist)
-bookName =  input("book: " ).strip()
-chapterNumber = int(input("from chapter: ").strip())
-totalChapters = len(booksdata[bookName])
-result_filename = bookName + str(chapterNumber) + "-" + str(totalChapters+chapterNumber-1)  + ".txt"
-book = booksdata[bookName]
-dict = {}
 
-for x in range(totalChapters):
-	chapter = book[chapterNumber-1][str(chapterNumber)]
-	print(chapter)
-	string = " ".join(chapter)
-	array = string.split(" ")
-	for word in array:
-	  word = word.replace(",", "")
-	  word = word.replace("[", "")
-	  word = word.replace("]", "")
-	  word = word.replace("!", "")
-	  word = word.replace(";", "")
-	  word = word.replace(":", "")
-	  word = word.replace(".", "")
-	  if word!="":
-	   if not word in dict:
-	     dict[word]= 1
-	   else:
-	     dict[word]+= 1
-	chapterNumber = chapterNumber + 1
-s = [(k, dict[k]) for k in sorted(dict, key=dict.get, reverse=True)]
-#result_filename = bookName + str(chapterNumber) + "-" + str(chapterNumber+totalChapters-1) + ".txt"
-new = open(result_filename, "w")
-new.write("")
-new.close()
-f = open(result_filename, "a")
+for bookName in booklist:
+    chapterNumber = 1
+    totalChapters = len(booksdata[bookName])
+    result_filename = bookName + str(chapterNumber) + "-" + str(totalChapters+chapterNumber-1)  + ".txt"
+    book = booksdata[bookName]
+    dict = {}
 
-newwords = []
-fj = open("oldwords.json")
-string = fj.read()
-json_data = json.loads(string)
-fj.close()
-for k,v in s:
-    if k in json_data:
-      print("already learned")
-      json_data[k] = json_data[k] + v
-    else:  
-      json_data[k] = v
-      string = k + ": " + str(v) + "\n"
-      f.write(string)
-      newwords.append(k)
+    for x in range(totalChapters):
+	    chapter = book[chapterNumber-1][str(chapterNumber)]
+	    print(chapter)
+	    string = " ".join(chapter)
+	    array = string.split(" ")
+	    for word in array:
+	      word = word.replace(",", "")
+	      word = word.replace("[", "")
+	      word = word.replace("]", "")
+	      word = word.replace("!", "")
+	      word = word.replace(";", "")
+	      word = word.replace(":", "")
+	      word = word.replace(".", "")
+	      if word!="":
+	       if not word in dict:
+	         dict[word]= 1
+	       else:
+	         dict[word]+= 1
+	    chapterNumber = chapterNumber + 1
+    s = [(k, dict[k]) for k in sorted(dict, key=dict.get, reverse=True)]
+    #result_filename = bookName + str(chapterNumber) + "-" + str(chapterNumber+totalChapters-1) + ".txt"
+    new = open(result_filename, "w")
+    new.write("")
+    new.close()
+    f = open(result_filename, "a")
 
-f.write("\ntotal words: " + str(len(newwords)))
-f.close()
+    newwords = []
+    fj = open("oldwords.json")
+    string = fj.read()
+    json_data = json.loads(string)
+    fj.close()
+    for k,v in s:
+        if k in json_data:
+          print("already learned")
+          json_data[k] = json_data[k] + v
+        else:  
+          json_data[k] = v
+          string = k + ": " + str(v) + "\n"
+          f.write(string)
+          newwords.append(k)
 
-with open("oldwords.json", "w") as outfile:
-  json.dump(json_data, outfile)
+    f.write("\ntotal words: " + str(len(newwords)))
+    f.close()
 
-add.converToJson(result_filename)  
-jsonfilename = result_filename[:-4] + ".json"
-print(jsonfilename)
-add.addGroup(jsonfilename)
+    with open("oldwords.json", "w") as outfile:
+      json.dump(json_data, outfile)
+
+    add.converToJson(result_filename)  
+    jsonfilename = result_filename[:-4] + ".json"
+    print(jsonfilename)
+    add.addGroup(jsonfilename)
